4e5cd47073b8ca4dc2373f560ec61fe37fec57d9
김병곤 <fharenheit@gmail.com>
2024-11-20 02:56:07 +0900

    'Init'

---
+++ src/main/java/io/datadynamics/prometheus/metricfilter/PrometheusMetricFilterApplication.java
@@ -0,0 +1,13 @@
+package io.datadynamics.prometheus.metricfilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrometheusMetricFilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrometheusMetricFilterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/datadynamics/prometheus/metricfilter/PrometheusMetricFilterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.datadynamics.prometheus.metricfilter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrometheusMetricFilterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
